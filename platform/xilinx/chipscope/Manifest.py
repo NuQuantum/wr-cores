9b302f9c2631ccdc6768fa5d2634306482ccd33e
Maciej Lipinski <maciej.lipinski@cern.ch>
2018-09-06 20:41:18 +0200

    [Virtex5 support] added chipscope for Virtex 5
    Note that on VXS switch, the last device (id 2) has address width 8 bits
    (this is needed to be inputed correctly when asked by chipscope)

--- platform/xilinx/chipscope/Manifest.py
+++ platform/xilinx/chipscope/Manifest.py
@@ -1,1 +1,4 @@
-files =["chipscope_icon.ngc",  "chipscope_ila.ngc" ]
+files =["chipscope_icon.ngc",
+        "chipscope_ila.ngc",
+        "chipscope_virtex5_icon.ngc",
+        "chipscope_virtex5_ila.ngc" ]
