0c08bdb288e446afd246eb0696b92fbbedfaaf0a
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2019-03-06 18:28:25 +0100

    nic_bw_throttling: update simulation manifest to use new hdlmake

--- testbench/nic_bw_throttling/Manifest.py
+++ testbench/nic_bw_throttling/Manifest.py
@@ -1,10 +1,14 @@
 target = "xilinx"
 action = "simulation"
+sim_tool = "modelsim"
+top_module = "main"
 syn_device = "XC6VLX130T"
 fetchto = "../../ip_cores"
 vlog_opt = "+incdir+../../sim +incdir+../../sim/wr-hdl"
 
 files = [ "main.sv" ]
+
+include_dirs = [ "../../sim" ]
 
 modules = { "local" : ["../../top/bare_top",
                        "../../ip_cores/general-cores",
