c6a8b74d8e8684ea7771b1c16670eec8a231641f
Maciej Lipinski <maciej.lipinski@cern.ch>
2020-04-09 18:43:07 +0200

    [CUTE] move oserdes_4_to_1 to board/cute (temporary solution)
    oserdes_4_to_1.vhd is Xilinx-specific and cannot be in modules/
    that include generic modules. This prevented the Altera-based
    design from building. Ultimately, the module needs to be moved
    to some more appropriate place, possibly to
    general-cores/platform/xilinx. To be decided. A temporary solution
    is to have it in board/cute, since it is only used by in the
    xwrc_board_cute.

--- modules/timing/Manifest.py
+++ modules/timing/Manifest.py
@@ -3,7 +3,6 @@ files = ["dmtd_phase_meas.vhd",
          "multi_dmtd_with_deglitcher.vhd",
          "hpll_period_detect.vhd",
          "pulse_gen.vhd",
-         "oserdes_4_to_1.vhd",
          "pulse_stamper.vhd",
          "pulse_stamper_sync.vhd",
          "dmtd_sampler.vhd" ]
