d1d1cc497419d396371f2da4ff1b8288101b216a
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2020-09-16 14:16:17 +0200

    platform/xilinx: select appropriate platform based on the FPGA family, not synthesis tool

--- platform/xilinx/Manifest.py
+++ platform/xilinx/Manifest.py
@@ -1,5 +1,6 @@
-if syn_tool=="vivado":
+if (syn_device[0:4].upper()=="XC7A" or syn_device[0:4].upper()=="XC7K" or
+        syn_device[0:4].upper()=="XCZU"):
 	files = [ "wr_xilinx_pkg.vhd", "xwrc_platform_vivado.vhd" ]
-elif syn_tool=="ise":
+else:
 	files = [ "wr_xilinx_pkg.vhd", "xwrc_platform_xilinx.vhd" ]
 modules = {"local" : ["wr_gtp_phy", "chipscope"]}
