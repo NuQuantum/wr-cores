04347b2760c7a94ca1bc1f0c519741407e55aa0c
CI <ci@ci.com>
2022-04-11 18:20:55 +0200

    Testbench streamers-on-spec-trigger-distribution now uses Riviera-Pro

--- testbench/wr_streamers/streamers-on-spec_trigger-distribution/Manifest.py
+++ testbench/wr_streamers/streamers-on-spec_trigger-distribution/Manifest.py
@@ -1,18 +1,20 @@
 action= "simulation"
 target= "xilinx"
 syn_device="xc6slx45t"
-sim_tool="modelsim"
+#sim_tool="modelsim"
+sim_tool="riviera"
 top_module="main"
 
 fetchto="../../ip_cores"
 vlog_opt="+incdir+../../../sim +incdir"
-
+vcom_opt="-relax -packagevhdlsv"
 include_dirs = ["../../../ip_cores/general-cores/modules/wishbone/wb_lm32/src" ]
 
 modules = { "local" : ["../../..",
                        "../../../modules/wr_streamers",
                        "../../../top/spec_1_1/wr_streamers_demo",
-                       "../../../ip_cores/general-cores"]}
+                       "../../../ip_cores/general-cores",
+                       "../../../ip_cores/urv-core"]}
 
 files = ["main.sv","synthesis_descriptor.vhd"]
 
--- testbench/wr_streamers/streamers-on-spec_trigger-distribution/run_ci_riv.do
+++ testbench/wr_streamers/streamers-on-spec_trigger-distribution/run_ci_riv.do
@@ -1,9 +1,10 @@
 # make -f Makefile > /dev/null 2>&1
-vsim -L unisim -L secureip work.main -voptargs="+acc" 
+vsim -L unisim -L secureip work.main +access +r 
 set NumericStdNoWarnings 1
 set StdArithNoWarnings 1
 
-do wave.do
+do wave_ci.do
 run 40000us
 wave zoomfull
 radix -hex
+
