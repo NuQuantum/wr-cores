2c9029a410b1b525dd9ee80bc46f69b5e98b666a
Jonah Foley <jonah.foley@nu-quantum.com>
2024-08-29 10:50:57 +0100

    feature: first pass impl of kasli board level
    feature: adds a range of core files

---
+++ board/kasli/Manifest.py
@@ -0,0 +1,12 @@
+files = [
+    "wr_kasil_pkg.vhd",
+    "xwrc_board_kasli.vhd",
+    "wrc_board_kasli.vhd",
+    "wrc_board_kasli_ip.xdc"
+]
+
+modules = {
+    "local" : [
+        "../common",
+    ]
+}
